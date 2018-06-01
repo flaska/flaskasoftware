99ee259d5914e7a2c28335a7ac711038d11b5db1
Jakub Flaska <jakub.flaska@nih.gov>
2018-06-01 17:42:32 -0400

    x

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express'),
 path = require('path');
 const app = express();
 
-app.use(['/'], /*express.static('./frontend/dist/frontend')*/ express.static(path.join(__dirname, './frontend/dist/frontend')));
+app.use(['/'], /*express.static('./frontend/dist/frontend')*/ express.static(path.join(__dirname, 'frontend','dist','frontend')));
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
