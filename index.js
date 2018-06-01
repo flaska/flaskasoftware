455bafd2bb871e980e8564c0cc5e5343ef23e1fc
Jakub Flaska <jakub.flaska@nih.gov>
2018-06-01 17:31:40 -0400

    xx

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express();
 
 app.use(['/'], express.static('./frontend/dist/frontend'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/dist/frontend/index.html'));
+});
+
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
