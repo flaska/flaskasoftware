549ded5b8236533f97b05cb1f3273425d2bcd7b2
Jakub Flaska <jakub.flaska@nih.gov>
2018-06-04 16:52:43 -0400

    x

--- public/main.js
+++ public/main.js
@@ -41,7 +41,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--<mat-accordion>-->\n  <!--<mat-expansion-panel>-->\n    <!--<mat-expansion-panel-header>-->\n      <!--<mat-panel-title>-->\n        <!--Who we are?-->\n      <!--</mat-panel-title>-->\n    <!--</mat-expansion-panel-header>-->\n  <!--</mat-expansion-panel>-->\n\n  <!--<mat-expansion-panel>-->\n    <!--<mat-expansion-panel-header>-->\n      <!--<mat-panel-title>-->\n        <!--What we do?-->\n      <!--</mat-panel-title>-->\n    <!--</mat-expansion-panel-header>-->\n  <!--</mat-expansion-panel>-->\n\n<!--</mat-accordion>-->\n\n<div class=\"scene\">\n  <div class=\"cube\">\n    <div class=\"cube__face cube__face--front\">code</div>\n    <div class=\"cube__face cube__face--back\">makes</div>\n    <div class=\"cube__face cube__face--right\">house</div>\n    <div class=\"cube__face cube__face--left\">difference</div>\n    <div class=\"cube__face cube__face--top\"></div>\n    <div class=\"cube__face cube__face--bottom\"></div>\n  </div>\n</div>\n"
+module.exports = "<cube></cube>\n\n<br>\n\n<pages></pages>\n"
 
 /***/ }),
 
@@ -95,6 +95,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 /* harmony import */ var _material_material_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../material/material.module */ "./src/material/material.module.ts");
+/* harmony import */ var _cube_cube_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./cube/cube.component */ "./src/app/cube/cube.component.ts");
+/* harmony import */ var _pages_pages_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./pages/pages.component */ "./src/app/pages/pages.component.ts");
+/* harmony import */ var _pages_mission_mission_page_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./pages/mission/mission.page.component */ "./src/app/pages/mission/mission.page.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -105,13 +108,19 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 
 
 
+
+
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
             declarations: [
-                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]
+                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"],
+                _cube_cube_component__WEBPACK_IMPORTED_MODULE_4__["CubeComponent"],
+                _pages_pages_component__WEBPACK_IMPORTED_MODULE_5__["PagesComponent"],
+                _pages_mission_mission_page_component__WEBPACK_IMPORTED_MODULE_6__["MissionPageComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
@@ -122,6 +131,153 @@ var AppModule = /** @class */ (function () {
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/cube/cube.component.html":
+/*!******************************************!*\
+  !*** ./src/app/cube/cube.component.html ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"scene\">\r\n  <div class=\"cube\">\r\n    <div class=\"cube__face cube__face--front\">code</div>\r\n    <div class=\"cube__face cube__face--back\">makes</div>\r\n    <div class=\"cube__face cube__face--right\">house</div>\r\n    <div class=\"cube__face cube__face--left\">difference</div>\r\n    <div class=\"cube__face cube__face--top\"></div>\r\n    <div class=\"cube__face cube__face--bottom\"></div>\r\n  </div>\r\n</div>\r\n\r\n<h3 class=\"name\">code | house</h3>\r\n<div class=\"motto\">Let's make a difference together...</div>\r\n\r\n"
+
+/***/ }),
+
+/***/ "./src/app/cube/cube.component.scss":
+/*!******************************************!*\
+  !*** ./src/app/cube/cube.component.scss ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".scene {\n  width: 200px;\n  height: 200px;\n  -webkit-perspective: 600px;\n          perspective: 600px;\n  margin: 70px auto; }\n\n.cube {\n  width: 100%;\n  height: 100%;\n  position: relative;\n  -webkit-transform-style: preserve-3d;\n          transform-style: preserve-3d;\n  transition: -webkit-transform 1s;\n  transition: transform 1s;\n  transition: transform 1s, -webkit-transform 1s;\n  -webkit-animation: spin 15s infinite linear;\n          animation: spin 15s infinite linear; }\n\n.cube__face {\n  position: absolute;\n  width: 200px;\n  height: 200px;\n  line-height: 200px;\n  text-align: center;\n  opacity: 1;\n  border: 1px solid white;\n  background-color: #039BE5;\n  color: white;\n  font-size: 3em; }\n\n.cube__face--front {\n  -webkit-transform: rotateY(0deg) translateZ(100px);\n          transform: rotateY(0deg) translateZ(100px); }\n\n.cube__face--right {\n  -webkit-transform: rotateY(90deg) translateZ(100px);\n          transform: rotateY(90deg) translateZ(100px); }\n\n.cube__face--back {\n  -webkit-transform: rotateY(180deg) translateZ(100px);\n          transform: rotateY(180deg) translateZ(100px); }\n\n.cube__face--left {\n  -webkit-transform: rotateY(-90deg) translateZ(100px);\n          transform: rotateY(-90deg) translateZ(100px);\n  background-color: #AEEA00; }\n\n.cube__face--top {\n  -webkit-transform: rotateX(90deg) translateZ(100px);\n          transform: rotateX(90deg) translateZ(100px); }\n\n.cube__face--bottom {\n  -webkit-transform: rotateX(-90deg) translateZ(100px);\n          transform: rotateX(-90deg) translateZ(100px); }\n\n@-webkit-keyframes spin {\n  from {\n    -webkit-transform: rotateY(0);\n            transform: rotateY(0); }\n  to {\n    -webkit-transform: rotateY(-360deg);\n            transform: rotateY(-360deg); } }\n\n.name {\n  text-align: center;\n  color: #039BE5;\n  font-size: 3em; }\n\n.motto {\n  text-align: center;\n  font-size: 1em; }\n"
+
+/***/ }),
+
+/***/ "./src/app/cube/cube.component.ts":
+/*!****************************************!*\
+  !*** ./src/app/cube/cube.component.ts ***!
+  \****************************************/
+/*! exports provided: CubeComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CubeComponent", function() { return CubeComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var CubeComponent = /** @class */ (function () {
+    function CubeComponent() {
+    }
+    CubeComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'cube',
+            template: __webpack_require__(/*! ./cube.component.html */ "./src/app/cube/cube.component.html"),
+            styles: [__webpack_require__(/*! ./cube.component.scss */ "./src/app/cube/cube.component.scss")]
+        })
+    ], CubeComponent);
+    return CubeComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/pages/mission/mission.page.component.html":
+/*!***********************************************************!*\
+  !*** ./src/app/pages/mission/mission.page.component.html ***!
+  \***********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<i class=\"fas fa-wrench\"></i> You have have a great idea? We can take care of the <strong>technical</strong> side...<br>\r\n  E-shop, search engine, database, mobile app...? We did it all...\r\n\r\n<br><br>\r\n\r\n<i class=\"fas fa-trophy\"></i> We're a team of highly motivated engineers and we'll treat your project like our own baby\r\nIt's like sports. There's many average Joes. But everyone will remember only the season's winner. We want to help you win.\r\n\r\n<br><br>\r\n\r\n<i class=\"fas fa-flask\"></i> What's our secret cocktail? We care! Coding is our hobby.\r\nWe stay up to date with state-of-the-art technologies. It's important for us to innovate because it saves your budget. It's like gunpowder vs a bow.\r\n"
+
+/***/ }),
+
+/***/ "./src/app/pages/mission/mission.page.component.ts":
+/*!*********************************************************!*\
+  !*** ./src/app/pages/mission/mission.page.component.ts ***!
+  \*********************************************************/
+/*! exports provided: MissionPageComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MissionPageComponent", function() { return MissionPageComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var MissionPageComponent = /** @class */ (function () {
+    function MissionPageComponent() {
+    }
+    MissionPageComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'mission-page',
+            template: __webpack_require__(/*! ./mission.page.component.html */ "./src/app/pages/mission/mission.page.component.html")
+        })
+    ], MissionPageComponent);
+    return MissionPageComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/pages/pages.component.html":
+/*!********************************************!*\
+  !*** ./src/app/pages/pages.component.html ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<mat-accordion>\r\n  <mat-expansion-panel>\r\n    <mat-expansion-panel-header>\r\n      <mat-panel-title>\r\n        Mission\r\n      </mat-panel-title>\r\n    </mat-expansion-panel-header>\r\n    <mission-page></mission-page>\r\n  </mat-expansion-panel>\r\n\r\n  <mat-expansion-panel>\r\n    <mat-expansion-panel-header>\r\n      <mat-panel-title>\r\n        Pricing\r\n      </mat-panel-title>\r\n    </mat-expansion-panel-header>\r\n    <!--<pricing></pricing>-->\r\n  </mat-expansion-panel>\r\n\r\n</mat-accordion>\r\n"
+
+/***/ }),
+
+/***/ "./src/app/pages/pages.component.ts":
+/*!******************************************!*\
+  !*** ./src/app/pages/pages.component.ts ***!
+  \******************************************/
+/*! exports provided: PagesComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PagesComponent", function() { return PagesComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var PagesComponent = /** @class */ (function () {
+    function PagesComponent() {
+    }
+    PagesComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'pages',
+            template: __webpack_require__(/*! ./pages.component.html */ "./src/app/pages/pages.component.html")
+        })
+    ], PagesComponent);
+    return PagesComponent;
 }());
 
 
@@ -212,13 +368,15 @@ var MaterialModule = /** @class */ (function () {
                 _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatButtonModule"],
                 _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatCardModule"],
                 _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatExpansionModule"],
-                _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_2__["BrowserAnimationsModule"]
+                _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_2__["BrowserAnimationsModule"],
+                _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatListModule"]
             ],
             exports: [
                 _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatButtonModule"],
                 _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatCardModule"],
                 _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatExpansionModule"],
-                _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_2__["BrowserAnimationsModule"]
+                _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_2__["BrowserAnimationsModule"],
+                _angular_material__WEBPACK_IMPORTED_MODULE_1__["MatListModule"]
             ],
         })
     ], MaterialModule);
