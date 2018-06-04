fa2db2d91ce91c6df882269d12f9282315396b4b
Jakub Flaska <jakub.flaska@nih.gov>
2018-06-04 17:08:09 -0400

    done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express');
+const express = require('express'),
+    compression = require('compression');
 const app = express();
 
 app.use(express.static('public'));
+
+app.use(compression());
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
